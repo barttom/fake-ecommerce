5e22a40fd98fff858984703c2f20d6b264aba998
bartektomczyk <bartlomiej.tomczyk@callstack.com>
2023-07-10 07:52:49 +0200

    feat: added icons to tabs

--- src/common/components/Navigator/TabNavigator.tsx
+++ src/common/components/Navigator/TabNavigator.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import {ProductsScreen} from '../../../feature/Products/';
 import {CartScreen} from '../../../feature/Cart/';
@@ -10,9 +11,33 @@ const TabStack = createMaterialBottomTabNavigator<TabStackParamList>();
 export const TabNavigator = () => {
   return (
     <TabStack.Navigator>
-      <TabStack.Screen name="Products" component={ProductsScreen} />
-      <TabStack.Screen name="Cart" component={CartScreen} />
-      <TabStack.Screen name="Settings" component={SettingsScreen} />
+      <TabStack.Screen
+        name="Products"
+        component={ProductsScreen}
+        options={{
+          tabBarIcon: ({color}) => (
+            <MaterialIcons name="list" color={color} size={24} />
+          ),
+        }}
+      />
+      <TabStack.Screen
+        name="Cart"
+        component={CartScreen}
+        options={{
+          tabBarIcon: ({color}) => (
+            <MaterialIcons name="shopping-basket" color={color} size={24} />
+          ),
+        }}
+      />
+      <TabStack.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({color}) => (
+            <MaterialIcons name="settings" color={color} size={24} />
+          ),
+        }}
+      />
     </TabStack.Navigator>
   );
 };
