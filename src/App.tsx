5154639cc7b7e4d47efc4c47415f511b49d843c6
bartektomczyk <bartlomiej.tomczyk@callstack.com>
2023-07-28 20:51:23 +0200

    chore: add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
-import {PaperProvider} from 'react-native-paper';
+import {PaperProvider, MD3LightTheme, MD3DarkTheme} from 'react-native-paper';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
 import {RootNavigator} from './common/components/Navigator';
 import {store} from './common/redux';
+import {colorsDark, colorsLight} from './common/theme/colors';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
+  const theme = isDarkMode
+    ? {...MD3DarkTheme, colors: colorsDark}
+    : {...MD3LightTheme, colors: colorsLight};
+
   return (
     <Provider store={store}>
-      <PaperProvider>
+      <PaperProvider theme={theme}>
         <SafeAreaView style={{flex: 1}}>
           <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
           <NavigationContainer>
