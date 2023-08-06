5f7d303a05553c700a23f115f6a9e598842c2fca
bartektomczyk <bartlomiej.tomczyk@callstack.com>
2023-08-06 18:05:03 +0200

    test: fixed app test

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -6,11 +6,12 @@ import 'react-native';
 import React from 'react';
 import {render, screen} from '@testing-library/react-native';
 import App from '../src/App';
+jest.useFakeTimers();
 
 // Note: test renderer must be required after react-native.
 
 it('renders correctly', () => {
   render(<App />);
 
-  expect(screen.getByText('Home')).toBeDefined();
+  expect(screen.getAllByText('Products')).toBeDefined();
 });
