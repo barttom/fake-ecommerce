e725fc8e520a576dc67606f862ea4364d0197a1f
Bartlomiej Tomczyk <bartlomiejtomczyk@gmail.com>
2023-10-12 14:33:59 +0200

    feat: addad proper colour for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import {
-  LogBox,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  useColorScheme,
-} from 'react-native';
+import {LogBox, StatusBar, StyleSheet, useColorScheme} from 'react-native';
 import {PaperProvider, MD3LightTheme, MD3DarkTheme} from 'react-native-paper';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import {RootNavigator} from './common/components/Navigator';
 import {initialStore} from './common/redux';
 import {colorsDark, colorsLight} from './common/theme/colors';
@@ -18,20 +13,34 @@ LogBox.ignoreAllLogs(true);
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
-
+  const colors = isDarkMode ? colorsDark : colorsLight;
   const theme = isDarkMode
-    ? {...MD3DarkTheme, colors: colorsDark}
-    : {...MD3LightTheme, colors: colorsLight};
+    ? {...MD3DarkTheme, colors}
+    : {...MD3LightTheme, colors};
+
+  const navigationTheme = {
+    dark: isDarkMode,
+    colors: {
+      primary: colors.primary,
+      background: colors.background,
+      card: colors.background,
+      text: colors.onBackground,
+      border: colors.outline,
+      notification: colors.onBackground,
+    },
+  };
 
   return (
     <Provider store={initialStore}>
       <PaperProvider theme={theme}>
-        <SafeAreaView style={styles.container}>
+        <SafeAreaView
+          style={[styles.container, {backgroundColor: colors.background}]}>
           <StatusBar
             barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-            backgroundColor={theme.colors.surface}
+            translucent
           />
-          <NavigationContainer>
+
+          <NavigationContainer theme={navigationTheme}>
             <RootNavigator />
           </NavigationContainer>
         </SafeAreaView>
