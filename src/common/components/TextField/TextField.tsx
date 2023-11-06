c812cf4823d342227bd46f83c196fc2bac37c53b
bartektomczyk <bartlomiejtomczyk@gmail.com>
2023-11-06 16:45:44 +0100

    feat: added textfield component

---
+++ src/common/components/TextField/TextField.tsx
@@ -0,0 +1,21 @@
+import React, {useMemo} from 'react';
+import {HelperText, TextInput, TextInputProps} from 'react-native-paper';
+
+export type TextFieldProps = Omit<TextInputProps, 'error'> & {error?: string};
+
+export const TextField = ({error, ...props}: TextFieldProps) => {
+  const isError = useMemo(() => !!error, [error]);
+
+  return (
+    <>
+      <TextInput
+        accessibilityLabel={`${props.label} input field`}
+        error={isError}
+        {...props}
+      />
+      <HelperText type="error" visible={isError}>
+        {error}
+      </HelperText>
+    </>
+  );
+};
